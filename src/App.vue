baf6a7705661aa8cc4a259c1710b0b585eb3c162
yuanbingbing <yuanbing1111bing@sina.com>
2018-03-21 10:53:37 +0800

    创建了左侧导航栏组件和头部的公共组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,13 +11,4 @@ export default {
 </script>
 
 <style lang="scss" scoped="" type="text/css">
-$color:'red';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #{$color};
-  margin-top: 60px;
-}
 </style>
